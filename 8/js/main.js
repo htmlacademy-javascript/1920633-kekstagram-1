546e9cc3b319210f2b33817305f6c27bdb5dab35
Keks <keks@htmlacademy.ru>
2024-05-14 21:37:13 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console*/
+import './util.js';
+import {createPostList} from './data.js';
+import './thumbnails.js';
+import './user-modal.js';
+
+console.log(createPostList());
