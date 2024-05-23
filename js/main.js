582ddffbd9b923c0e9fb774dd2a96195b9e3cb53
Kristina Lingthep <kris.lingthep@gmail.com>
2024-05-23 20:59:35 +0300

    Добавляет форму редактирования изображения и валидацию

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ import './util.js';
 import {createPostList} from './data.js';
 import './thumbnails.js';
 import './user-modal.js';
+import './user-form.js';
+import './validate-photo-editor.js';
+import './scale-image.js';
+import './filters.js';
 
 console.log(createPostList());
